a82ed070e627b72811367514afd9107c08350659
Paul Millband <millband_paul@hotmail.co.uk>
2016-09-06 08:00:01 +0100

    add some code comments

--- src/code/authenticationAndSecurity.php
+++ src/code/authenticationAndSecurity.php
@@ -1,17 +1,50 @@
 <?php
 
+/***
+ * Class authenticationAndSecurity security
+ */
 class authenticationAndSecurity {
+    /**
+     * redirect back to the homepage
+     */
+    function redirectBackToIndex(){
+        $url = (string)filter_input(INPUT_SERVER,'REQUEST_SCHEME').'://'.(string)filter_input(INPUT_SERVER,'HTTP_HOST').(string)filter_input(INPUT_SERVER,'REQUEST_URI');
+        $url = explode('/',$url);
+        array_pop( $url );
+        array_pop( $url );
+        $url = implode('/', $url );
+        header( "Location: $url/index.php",true, 302 ); // cant use other than 301 and 302 or browsers don't redirect
+        exit;
+    }
+
+    /**
+     *  redirect to the index page if the user is not logged in
+     */
     function redirectIfNotLoggedIn(){
         $authentication = $this->getAuthentication();
         if( $authentication['type']==='cookie' && $authentication['details'] === false ){
             $this->redirectBackToIndex();
         }
     }
-    
+
+    /**
+     * set the session cookie
+     * @param string $name cookie name
+     * @param string $value cookie value
+     * @param int $expire expiry Unix timestamp
+     * @param string $path The path on the server in which the cookie will be available on
+     */
     function setCookie( $name, $value, $expire = 0, $path = '/'){
         $encryptedValue = $this->encryptDecrypt('encrypt', $value);
         setCookie($name, $encryptedValue, $expire, $path);
     }
+
+    /**
+     * encrypt or decrypt string
+     * @param string $action encrypt|decrypt
+     * @param string $string subject
+     * @return string encrypted/decrypted string
+     */
     function encryptDecrypt($action, $string) {
         $output = false;
         $key = $GLOBALS['cookieEncryptionKey'];
@@ -26,11 +59,20 @@ class authenticationAndSecurity {
         }
         return $output;
     }
-    
+
+    /**
+     * get a decrypted cookie value
+     * @param $cookieName
+     * @return string
+     */
     function getSingleCookie($cookieName){
         $authenticationAndSecurity = new authenticationAndSecurity;
         return $this->encryptDecrypt( 'decrypt', $authenticationAndSecurity->getcookie($cookieName) );
     }
+    /**
+     * get the cookies values extracted
+     * @return null|array array of cookie data
+     */
     function getBrowserCookies(){
         $authenticationAndSecurity = new authenticationAndSecurity;
         if( null === $authenticationAndSecurity->getCookie('Set-Cookie0') || $authenticationAndSecurity->getCookie('Set-Cookie0')===null){
@@ -43,6 +85,11 @@ class authenticationAndSecurity {
         }
         return $cookies;
     }
+    /**
+     * explode cookie value
+     * @param string $string decrypted cookie value
+     * @return array cookie value's data
+     */
     function splitCookie($string){
         $stringComponents = explode(';',$string);
         foreach ($stringComponents as $value) {
@@ -54,6 +101,9 @@ class authenticationAndSecurity {
         }
         return $cookieData;
     }
+    /**
+     * remove the cookies
+     */
     function removeCookies(){
         // set the expiration date to one hour ago
         setCookie("Set-Cookie0", "", time() - 3600, '/');
@@ -61,6 +111,10 @@ class authenticationAndSecurity {
         $reporterCookieName = 'myCookie';
         setCookie($reporterCookieName, "", time() - 3600, '/');
     }
+    /**
+     * get the Youtrack authentication details
+     * @return array Youtrack authentication details
+     */
     function getAuthentication(){
         if( !isset($GLOBALS['authenticationType']) || $GLOBALS['authenticationType'] === null ){
             $authenticationType = 'cookie';
@@ -104,31 +158,12 @@ class authenticationAndSecurity {
     }
 
     /**
-     * @param int $code redirect code
-     */
-    function redirectBackToIndex(){
-        $url = (string)filter_input(INPUT_SERVER,'REQUEST_SCHEME').'://'.(string)filter_input(INPUT_SERVER,'HTTP_HOST').(string)filter_input(INPUT_SERVER,'REQUEST_URI');
-        $url = explode('/',$url);
-        array_pop( $url );
-        array_pop( $url );
-        $url = implode('/', $url );
-        header( "Location: $url/index.php",true, 302 ); // cant use other than 301 and 302 or browsers don't redirect
-        exit;
-    }
-    
-    function getAllPosts(){
-        $keys = array_keys($_POST);
-        $post = array_map(
-           function ($key) {
-               return filter_input(INPUT_POST, $key);
-           },
-           $keys
-        );
-        return array_combine($keys, $post);
-    }
-    
-    /*
+     * filter input for security
      * dosnt work with trying to return arrays
+     * @param string $type post|cookie|get
+     * @param string $varName name of the variable to filter
+     * @return false|null|string filtered string, or false if filter_input fails,
+     * or null if variable not set or no length
      */
     function filterInput($type,$varName){
         switch($type){
@@ -152,13 +187,46 @@ class authenticationAndSecurity {
             return null;
         }
     }
+    /**
+     * get all post data with input filtered
+     * @return array post data
+     */
+    function getAllPosts(){
+        $keys = array_keys($_POST);
+        $post = array_map(
+           function ($key) {
+               return filter_input(INPUT_POST, $key);
+           },
+           $keys
+        );
+        return array_combine($keys, $post);
+    }
+
+    /**
+     * get post value
+     * @param string $name name of the value
+     * @return false|null|string filtered string, or false if filter_input fails,
+     * or null if variable not set or no length
+     */
     function getPost($name){
         return $this->filterInput('post',$name);
     }
+    /**
+     * get cookie value
+     * @param string $name name of the value
+     * @return false|null|string filtered string, or false if filter_input fails,
+     * or null if variable not set or no length
+     */
     function getCookie($name){
-        return $this->filterInput('cookie',$name);            
+        return $this->filterInput('cookie',$name);
     }
+    /**
+     * get get value
+     * @param string $name name of the value
+     * @return false|null|string filtered string, or false if filter_input fails,
+     * or null if variable not set or no length
+     */
     function getGet($name){
-        return $this->filterInput('get',$name);            
+        return $this->filterInput('get',$name);
     }
 }
